1c777c272f947372108d81c2f728de213dde3490
Rukin Danil <rukin2018d@yandex.ru>
2021-05-14 18:03:19 +0700

    Метод трапеций

--- Model/Methods/TrapeziumMethod.cs
+++ Model/Methods/TrapeziumMethod.cs
@@ -10,14 +10,51 @@ namespace KantorLr9.Model.Methods
 	{
 		public override AutoStepMethodAnswer GetSolutionWithAutoStep(Func<double, double> function, double left, double right, double precision, double segmentNumber)
 		{
-			throw new NotImplementedException();
+			int steps = 0;
+			int stepsOnOneSegment;
+			double resultValue = 0;
+			double intervalSize = Math.Abs(right - left) / segmentNumber;
+			double currentLeft = left, currentRight = left + intervalSize;
+			for (int i = 0; i < segmentNumber; i++)
+			{
+				resultValue += Recalculate(function, currentLeft, currentRight, precision, out stepsOnOneSegment);
+				steps += stepsOnOneSegment;
+				currentLeft = currentRight;
+				currentRight += intervalSize;
+			}
+			AutoStepMethodAnswer answer = new AutoStepMethodAnswer(ToString(), resultValue, steps);
+			return answer;
 		}
+
+		private double Recalculate(Func<double, double> function, double left, double right, double precision, out int iters) 
+		{
+			double step = Math.Abs(left - right);
+			double result = -1, previousResult = 0;
+			iters = 0;
+			while (Math.Abs(result - previousResult) > precision)
+			{
+				iters = 0;
+				previousResult = result;
+				result = 0;
+				double x = left + step;
+				while (x < right)
+				{
+					result += step * function(x);
+					x += step;
+					iters++;
+				}
+				result += (step / 2) * (function(left) + function(right));
+				step /= 2;
+			}
+			return result;
+		}
+
 
 		public override double GetSolutionWithFixedStep(Func<double, double> function, double left, double right, double step)
 		{
 			double res = 0;
 			double x = left + step;
-			while (x < right) //должно быть как раз от 1 до n-1 
+			while (x < right) 
 			{
 				res += step * function(x);
 				x += step;
@@ -26,5 +63,7 @@ namespace KantorLr9.Model.Methods
 			return res;
 
 		}
+
+		public override string ToString() => "Метод трапеций";
 	}
 }
