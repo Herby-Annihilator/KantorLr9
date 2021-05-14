867b1207d7f655aecec38f351830952e1c9884bd
Rukin Danil <rukin2018d@yandex.ru>
2021-05-14 14:28:47 +0700

    Добавьте файлы проекта.

---
+++ Model/Services/ServiceRegistrator.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KantorLr9.Model.Services
+{
+	public static class ServiceRegistrator
+	{
+		public static void RegisterServices(this IServiceCollection services)
+		{
+			return;
+		}
+	}
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using KantorLr9.Infrastructure.Commands;
+using KantorLr9.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using System.Windows.Markup;
+
+namespace KantorLr9.ViewModels
+{
+	[MarkupExtensionReturnType(typeof(MainWindowViewModel))]
+	public class MainWindowViewModel : ViewModel
+	{
+		#region Properties
+		private string _title = "Title";
+		public string Title { get => _title; set => Set(ref _title, value); }
+		#endregion
+
+		#region Commands
+
+		#endregion
+	}
+}
